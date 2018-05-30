3b1a49d1c3c054760b4faa15a651c723245f2e6f
Christian Stigen Larsen <csl@csl.name>
2018-05-30 23:27:42 +0200

    Adds more bells and whistles

--- prowess.py
+++ prowess.py
@@ -45,116 +45,140 @@ class Game:
             s += [None]
         return s
 
-    def look(self):
-        write("%s." % self.obj.about)
-
-        if self.obj.objs:
-            write(" You see %s." % many(self.obj.objs.keys()))
-
-        if self.obj.exits:
-            def to(wo):
-                way, obj = wo
-                return "%s to the %s" % (obj.name, way)
-            write(" There is %s." % many(map(to, self.obj.exits.items())))
-
-        writeln("")
-
     def run(self):
         self.look()
         while True:
             self.dispatch(*self.parse(self.ask()))
 
-    def dispatch(self, verb, obj, subj):
-        if verb in ["l", "look", "examine"]:
-            if not obj:
-                self.look()
-            elif obj in self.obj.objs:
-                writeln("%s." % self.obj.objs[obj].about)
-            elif obj in self.inv.objs:
-                writeln("%s." % self.inv.objs[obj].about)
-            elif obj in (o.name for o in self.obj.exits.values()):
-                writeln("You can't see the %s from here." % obj)
-            elif obj in self.obj.exits:
-                writeln("There is %s to the %s." % (a(self.obj.exits[obj].name),
-                    obj))
-            elif obj in ["east", "north", "west", "south"]:
-                writeln("There isn't anything particular to the %s." % obj)
-            else:
-                writeln("There is no %s here." % obj)
-        elif verb in ["i", "inventory"]:
+    def look(self, obj=None):
+        if not obj:
+            write("%s." % self.obj.about)
+
+            if self.obj.objs:
+                write(" You see %s." % many(self.obj.objs.keys()))
+
+            if self.obj.exits:
+                def to(wo):
+                    way, obj = wo
+                    return "%s to the %s" % (obj.name, way)
+                write(" There is %s." % many(map(to, self.obj.exits.items())))
+
+            writeln("")
+        elif obj in self.obj.objs:
+            writeln("%s." % self.obj.objs[obj].about)
+        elif obj in self.inv.objs:
+            writeln("%s." % self.inv.objs[obj].about)
+        elif obj in (o.name for o in self.obj.exits.values()):
+            writeln("You can't see the %s from here." % obj)
+        elif obj in self.obj.exits:
+            writeln("There is %s to the %s." % (a(self.obj.exits[obj].name),
+                obj))
+        elif obj in ["east", "north", "west", "south"]:
+            writeln("There isn't anything particular to the %s." % obj)
+        else:
+            writeln("There is no %s here." % obj)
+
+    def inventory(self, obj):
+        if obj:
+            self.look(obj)
+        elif self.inv.objs:
             writeln("You are carrying %s." % many(self.inv.objs))
-        elif verb in ["g", "go", "walk"]:
-            if obj in self.obj.exits:
-                self.obj = self.obj.exits[obj]
-                self.look()
-                return
-            elif obj in (o.name for o in self.obj.exits.values()):
-                for o in self.obj.exits.values():
-                    if o.name == obj:
-                        self.obj = o
-                        self.look()
-                        return
-            writeln("You can't go there.")
-        elif verb in ["q", "quit"]:
-            raise StopIteration
-        elif verb in ["t", "take", "get"]:
-            if obj in self.obj.objs:
-                target = self.obj.objs.pop(obj)
-                self.inv.objs[obj] = target
-                writeln("You put the %s in your pocket." % obj)
-            else:
-                if obj in (o.name for o in self.obj.exits.values()):
-                    writeln("You cannot actually take the %s." % obj)
-                elif obj in self.obj.exits:
-                    writeln("Right, you can't pocket an abstract term like %r."
-                            % obj)
-                else:
-                    writeln("I see no %s here." % obj)
-        elif verb in ["d", "drop"]:
-            if obj in self.inv.objs:
-                target = self.inv.objs.pop(obj)
-                self.obj.objs[obj] = target
-                writeln("You drop the %s." % obj)
-            else:
-                writeln("You don't have %s." % a(obj))
-        elif verb:
-            # Perform action on an object
-            if obj in self.inv.objs:
-                target = self.inv.objs[obj]
-                if verb in target.actions:
-                    actions = target.actions[verb]
-                    if isinstance(actions, list):
-                        writeln(" ".join(actions))
-                    else:
-                        writeln(actions)
+        else:
+            writeln("You are carrying nothing.")
+
+    def go(self, obj):
+        if obj in self.obj.exits:
+            self.obj = self.obj.exits[obj]
+            self.look()
+            return
+        elif obj in (o.name for o in self.obj.exits.values()):
+            for o in self.obj.exits.values():
+                if o.name == obj:
+                    self.obj = o
+                    self.look()
                     return
-            if obj in self.obj.objs:
-                target = self.objs[obj]
-                if verb in target.actions:
-                    writeln(" ".join(target.actions[verb]))
-                    return
-            if verb in self.obj.actions:
-                actions = self.obj.actions[verb]
-                if isinstance(actions, list):
-                    writeln(" ".join(actions))
-                else:
-                    writeln(actions)
-                return
-            # Do any objects have this action?
-            found = []
-            for o in self.inv.objs.values():
-                if verb in o.actions:
-                    found.append(o.name)
-                    break
-            for o in self.obj.objs.values():
-                if verb in o.actions:
-                    found.append(o.name)
-                    break
-            if not found:
-                writeln("There is nothing to %s around here." % verb)
+        writeln("You can't go there.")
+
+    def quit(self, obj):
+        raise StopIteration
+
+    def take(self, obj):
+        if obj in self.obj.objs:
+            target = self.obj.objs.pop(obj)
+            self.inv.objs[obj] = target
+            writeln("You take the %s." % obj)
+        else:
+            if obj in (o.name for o in self.obj.exits.values()):
+                writeln("You cannot actually take the %s." % obj)
+            elif obj in self.obj.exits:
+                writeln("Right, you can't pocket an abstract term like %r."
+                        % obj)
             else:
-                writeln("What do you want to %s? %s?" % (verb,
-                    many(found, art=the, sep="or").capitalize()))
+                writeln("I see no %s here." % obj)
+
+    def drop(self, obj):
+        if obj in self.inv.objs:
+            target = self.inv.objs.pop(obj)
+            self.obj.objs[obj] = target
+            writeln("You drop the %s." % obj)
+        else:
+            writeln("You don't have %s." % a(obj))
+
+    def action(self, verb, obj):
+        # Perform action on an object
+        if obj in self.inv.objs:
+            target = self.inv.objs[obj]
+            if verb in target.actions:
+                writeln(target.actions[verb])
+                return
+        if obj in self.obj.objs:
+            target = self.obj.objs[obj]
+            if verb in target.actions:
+                writeln(target.actions[verb])
+                return
+        if verb in self.obj.actions:
+            actions = self.obj.actions[verb]
+            writeln(actions)
+            return
+        # Do any objects have this action?
+        found = []
+        for o in self.inv.objs.values():
+            if verb in o.actions:
+                found.append(o.name)
+                break
+        for o in self.obj.objs.values():
+            if verb in o.actions:
+                found.append(o.name)
+                break
+        if not found:
+            writeln("There is nothing to %s around here." % verb)
+        else:
+            writeln("What do you want to %s? %s?" % (verb,
+                many(found, art=the, sep="or").capitalize()))
+
+    def dispatch(self, verb, obj, subj):
+        funcs = {
+            "d": self.drop,
+            "drop": self.drop,
+            "g": self.go,
+            "go": self.go,
+            "i": self.inventory,
+            "inventory": self.inventory,
+            "l": self.look,
+            "look": self.look,
+            "q": self.quit,
+            "quit": self.quit,
+            "t": self.take,
+            "take": self.take,
+        }
+
+        if verb in funcs:
+            func = funcs[verb]
+            func(obj)
+            return
+
+        if verb:
+            self.action(verb, obj)
 
 
 class Object:
